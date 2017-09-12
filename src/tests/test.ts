5824943fed7d44110080d7644f80990fe7ac0cf3
Sean Johnson <sjohnson@sjohnsonaz.com>
2017-09-11 17:10:45 -0700

    Added tests.

--- src/tests/test.ts
+++ src/tests/test.ts
@@ -4,7 +4,58 @@ import Router from '../scripts/main';
 import { wait } from '../scripts/util/TestUtils';
 
 describe('Router', () => {
-    it('should handle the default route', async () => {
+    it('should run on listen', async () => {
+        window.location.hash = '';
+        let runCount = 0;
+        let router = new Router({
+            '': () => {
+                runCount++;
+            }
+        });
+        router.listen();
+
+        await wait(0);
+        router.stop();
+        expect(runCount).to.equal(1);
+    });
+
+    it('should handle a change from default to simple', async () => {
+        window.location.hash = '';
+        let runCount = 0;
+        let router = new Router({
+            'test': () => {
+                runCount++;
+            }
+        });
+        router.listen();
+
+        await wait(0);
+        window.location.hash = 'test';
+
+        await wait(0);
+        router.stop();
+        expect(runCount).to.equal(1);
+    });
+
+    it('should handle a change from simple to default', async () => {
+        window.location.hash = 'test';
+        let runCount = 0;
+        let router = new Router({
+            '': () => {
+                runCount++;
+            }
+        });
+        router.listen(true);
+
+        await wait(0);
+        window.location.hash = '';
+
+        await wait(0);
+        router.stop();
+        expect(runCount).to.equal(1);
+    });
+
+    it('should ignore a change from default to default', async () => {
         window.location.hash = '';
         let runCount = 0;
         let router = new Router({
@@ -15,19 +66,22 @@ describe('Router', () => {
         router.listen(true);
 
         await wait(0);
+        window.location.hash = '';
+
+        await wait(0);
         router.stop();
-        expect(runCount).to.equal(1);
+        expect(runCount).to.equal(0);
     });
 
-    it('should handle a simple route', async () => {
-        window.location.hash = 'abc';
+    it('should handle a change from simple to simple', async () => {
+        window.location.hash = 'abcd';
         let runCount = 0;
         let router = new Router({
             'test': () => {
                 runCount++;
             }
         });
-        router.listen();
+        router.listen(true);
 
         await wait(0);
         window.location.hash = 'test';
